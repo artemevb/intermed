02649c9e41562f914994013e570204fd3180954e
Andrey <aa71555443@gmail.com>
2024-08-09 11:33:05 +0500

    complete catalog

--- app/_components/Categories/CategoryItem.js
+++ app/_components/Categories/CategoryItem.js
@@ -9,7 +9,7 @@ export default function CategoryItem({ key, title, imageSrc, slug }) {
       className="w-full border overflow-hidden transition-all duration-200 relative mdl:pb-72 max-mdl:h-[180px] flex flex-row items-center gap-5 mdl:flex-col h-[300px] px-2 mdl:px-6 mdl:py-6 max-mdl:pt-[20px] max-mdl:pl-[15px]"
     >
       <div className="mdl:flex flex-col mdl:items-center max-mdl:max-w-[167px] flex items-start h-full ">
-        <h2 className="text-2xl max-mdx:text-2xl font-semibold mdl:text-center z-10 mx-auto max-mdl:mb-[5px]">{title}</h2>
+        <h2 className=" max-mdx:text-[18px] lh text-2xl mb-2 font-semibold mdl:text-center z-10 mx-auto max-mdl:mb-[5px]">{title}</h2>
         <Link href={`/categories/catalog/${slug}`}>
           <GreenArrow title={"Перейти"} />
         </Link>
@@ -20,7 +20,7 @@ export default function CategoryItem({ key, title, imageSrc, slug }) {
           width={500}
           height={500}
           alt={`${title} Photo`}
-          className="absolute h-full z-0 object-contain -bottom-10 mdl:-bottom-16 mdl:w-1/2 mdl:h-[80%] max-mdl:w-auto max-mdl:h-full max-mdl:right-0"
+          className="absolute h-full max-mdx:max-w-[157px] z-0 object-contain -bottom-10 mdl:-bottom-16 mdl:w-1/2 mdl:h-[80%] max-mdl:w-auto max-mdl:h-full max-mdl:right-0"
         />
       </div>
     </div>
