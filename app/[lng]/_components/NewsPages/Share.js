b892772534579b383a4f9c81cf17f72adb5485fc
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-08-30 15:50:49 +0500

    Fixed merge

--- app/[lng]/_components/NewsPages/Share.js
+++ app/[lng]/_components/NewsPages/Share.js
@@ -1,6 +1,8 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Image from 'next/image'
 import Link from 'next/link'
 import newsPhoto from '@/public/images/news/news-photo.png'
@@ -11,10 +13,14 @@ import instagr from '@/public/svg/inst.svg'
 import whatsapp from '@/public/svg/whats.svg'
 import axios from 'axios'
 import { useParams } from 'next/navigation'
+import { useTranslation } from '../../../i18n/client';
+import { useLanguage } from '../../../i18n/locales/LanguageContext';
 
 export default function NewsTitle() {
+	const lng = useLanguage();
+    const { t } = useTranslation(lng, 'news-pages-share')
 	const [news, setNews] = useState(null) // Initial state is set to null
-	const { lng, slug } = useParams()
+	const {slug } = useParams()
 	useEffect(() => {
 		const fetchNewsWithSlug = async () => {
 			try {
@@ -34,25 +40,32 @@ export default function NewsTitle() {
 	}, [lng, slug])
 
 	const icons = [
-		{ src: telegram, alt: 'Telegram Icon', link: '#' },
-		{ src: facebook, alt: 'Facebook Icon', link: '#' },
-		{ src: instagr, alt: 'Instagram Icon', link: '#' },
-		{ src: whatsapp, alt: 'WhatsApp Icon', link: '#' },
-	]
+        { src: telegram, alt: 'Telegram Icon', link: 'https://t.me/intermedtrade' },
+        { src: facebook, alt: 'Facebook Icon', link: 'https://www.facebook.com/intermed.mindray' },
+        { src: instagr, alt: 'Instagram Icon', link: 'https://www.instagram.com/intermed.mindray' },
+        { src: whatsapp, alt: 'WhatsApp Icon', link: '#' },
+    ];
 
 	const copyLinkToClipboard = () => {
-		navigator.clipboard
-			.writeText(window.location.href)
-			.then(() => {
-				alert('Ссылка скопирована!')
-			})
-			.catch(err => {
-				console.error('Ошибка при копировании ссылки: ', err)
-			})
-	}
+        navigator.clipboard.writeText(window.location.href)
+            .then(() => {
+                toast.success(t('link_copied'), {
+                    position: 'top-right',
+                    autoClose: 3000,
+                });
+            })
+            .catch(err => {
+                toast.error(t('copy_error'), {
+                    position: 'top-right',
+                    autoClose: 3000,
+                });
+                console.error(t('copy_error'), err);
+            });
+    };
 
 	return (
 		<div className='bg-slate-100'>
+			 <ToastContainer />
 			<div className='max-w-[1440px] mdl:mx-auto'>
 				<div className='mx-[15px] mdl:flex mdl:flex-row-reverse mdl:items-center mdl:justify-between xl:flex-row xl:justify-center relative  mdl:py-4'>
 					<div className='mdl:items-center mdl:flex  flex justify-center content-center'>
@@ -68,8 +81,7 @@ export default function NewsTitle() {
 					<div>
 						<div className='flex flex-col uppercase text-redMain  mdl:flex-1 pb-[24px] '>
 							<h2 className='z-20 mt-8 ml-3 font-semibold text-[27px] xl:text-[38px] xl:mb-3 xl:ml-9 mdl:text-[23px] slg:text-[30px]'>
-								Поделись этой
-								<br /> новостью с другом!
+							{t('share-1')}<br />{t('share-2')}
 							</h2>
 							<p className='text-gray-500 mt-2 text-[15px] normal-case mdl:w-[320px] xl:text-[20px] xl:w-[503px] z-20 xl:ml-9 ml-3 slg:text-[18px] xl:mt-1'>
 								Поделитесь этой новостью с коллегами и вместе улучшайте свои
@@ -95,7 +107,7 @@ export default function NewsTitle() {
 								onClick={copyLinkToClipboard}
 								className='hidden xl:flex items-center justify-center text-white text-[16px] xl:text-[20px] bg-[#E94B50] px-8 py-3 hover:bg-[#EE787C]'
 							>
-								копировать ссылку
+								  {t('copy-link')}
 							</button>
 							{icons.map((icon, index) => (
 								<div
@@ -111,6 +123,7 @@ export default function NewsTitle() {
 											width={21}
 											height={21}
 											quality={100}
+											target="_blank"
 											alt={icon.alt}
 											className='w-[21px] h-[21px] object-cover slg:h-[24px] slg:w-[24px]'
 										/>
