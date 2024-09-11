38d49514eb6108e41b98c520c085b1fb557f94b8
Andrey <aa71555443@gmail.com>
2024-09-11 14:07:32 +0500

    added google-tags-test

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -31,8 +31,28 @@ export default function RootLayout({
 }) {
     return (
         <html lang={lng} dir={dir(lng)}>
-            <head />
+            <head>
+                {/* Google Tag Manager */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PJ2PVRJC');
+                        `,
+                    }}
+                />
+                {/* Other head content */}
+            </head>
             <body>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ2PVRJC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                </noscript>
+                {/* Main Content */}
                 <LanguageProvider lng={lng}>
                     <Header lng={lng} />
                     <main className="w-full bg-white relative">{children}</main>
