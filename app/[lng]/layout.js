d4fe6f0d40a50b8b78b89a5ef0356ea89577d415
Andrey <aa71555443@gmail.com>
2024-09-13 18:40:58 +0500

    new google tag manager url

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PJ2PVRJC');
+})(window,document,'script','dataLayer','GTM-MDWVM3M');
                         `,
                     }}
                 />
@@ -48,7 +48,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <body>
                 <noscript>
                     <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-PJ2PVRJC"
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MDWVM3M"
                         height="0"
                         width="0"
                         style={{ display: 'none', visibility: 'hidden' }}
