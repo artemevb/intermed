5acbd119a1fff2f02c07050190ab1e25346238d9
Andrey <aa71555443@gmail.com>
2024-09-23 08:45:36 +0500

    updated middleware, added processing of static files

--- middleware.js
+++ middleware.js
@@ -5,29 +5,48 @@ import { fallbackLng, languages, cookieName } from './app/i18n/settings'
 acceptLanguage.languages(languages)
 
 export const config = {
-  // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)']
+  matcher: [
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)'
+  ],
 }
 
 export function middleware(req) {
-  let lng
-  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
-  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
-  if (!lng) lng = fallbackLng
+  const { pathname } = req.nextUrl
 
-  // Redirect if lng in path is not supported
+  // Исключаем статические файлы из обработки middleware
+  const isPublicFile = /\.(.*)$/.test(pathname)
+  if (isPublicFile) {
+    return NextResponse.next()
+  }
+
+  let lng
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName).value)
+  }
+  if (!lng) {
+    lng = acceptLanguage.get(req.headers.get('Accept-Language'))
+  }
+  if (!lng) {
+    lng = fallbackLng
+  }
+
+  // Перенаправляем, если путь не содержит поддерживаемый языковой префикс
   if (
-    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith('/_next')
+    !languages.some((loc) => pathname.startsWith(`/${loc}`)) &&
+    !pathname.startsWith('/_next')
   ) {
-    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
+    return NextResponse.redirect(new URL(`/${lng}${pathname}`, req.url))
   }
 
   if (req.headers.has('referer')) {
     const refererUrl = new URL(req.headers.get('referer'))
-    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    )
     const response = NextResponse.next()
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
+    if (lngInReferer) {
+      response.cookies.set(cookieName, lngInReferer)
+    }
     return response
   }
 
